68541c71d810fb8fd3b10018ed8af2ce2743f61b
Saidakbar_Pardaboyev <pardaboyevsaidakbar103@gmail.com>
2024-07-08 21:13:30 +0500

    completed middleware

---
+++ api/middleware/middleware.go
@@ -0,0 +1,38 @@
+package middleware
+
+import (
+	"api_gateway/api/token"
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func JWTMiddleware() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		auth := ctx.GetHeader("Authorizaiton")
+
+		if auth == "" {
+			ctx.AbortWithStatusJSON(http.StatusBadRequest,
+				fmt.Errorf("authorization header is required"))
+			return
+		}
+
+		valid, err := token.ValidateToken(auth)
+		if err != nil || !valid {
+			ctx.AbortWithStatusJSON(http.StatusBadRequest,
+				fmt.Errorf("invalid token: %s", err))
+			return
+		}
+
+		claims, err := token.ExtractClaims(auth)
+		if err != nil || !valid {
+			ctx.AbortWithStatusJSON(http.StatusBadRequest,
+				fmt.Errorf("invalid token claims: %s", err))
+			return
+		}
+
+		ctx.Set("claims", claims)
+		ctx.Next()
+	}
+}
