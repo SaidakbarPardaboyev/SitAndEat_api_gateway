3e415e8335fbd4dfdd5a66ebf90914f8cea6adae
pronium <azizbekqobulov05@gmail.com>
2024-07-10 19:29:50 +0500

    in progres..

--- api/handler/users.go
+++ api/handler/users.go
@@ -1,42 +1,63 @@
 package handler
 
 import (
-	user "api_gateway/genproto/users"
-	"context"
+	"api_gateway/genproto/users"
 	"net/http"
+
+	"github.com/golang-jwt/jwt"
+	_ "github.com/swaggo/swag"
 
 	"github.com/gin-gonic/gin"
 )
 
-// @Summary User profile ni ko'rsatish
-// @Description Profile malumotlarini olish
-// @ID User
-// @Produce json
-// @Success 200 {object} user.GetUser "Ok"
-// @Failure 400 {object} map[string]string "Bad Request"
-// @Failure 500 {object} map[string]string "Internal server error"
-// @Router //getProfile/:id [get]
-func (h *Handler) GetProfile(c *gin.Context) {
-	req := user.UserId{}
-	if err := c.ShouldBindJSON(&req); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{
-			"error": "Error reading from body",
+func (h *Handler) GetProfileById(c *gin.Context) {
+	claims,exists := c.Get("claims")
+
+	if !exists {
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+			"error": "Claims not found",
 		})
 		return
 	}
+	id := c.Param("id")
 
-	resp, err := h.UserClient.GetProfile(context.Background(), &req)
+	req := &users.UserId{
+		UserId: id,
+	}
+
+	res, err := h.grpcClient.GetProfileById(c, req)
 	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
 			"error": err.Error(),
 		})
 		return
 	}
-	c.JSON(http.StatusOK,resp)
+	c.JSON(http.StatusOK, res)
 }
 
-func (h *Handler) UpdateProfile(c *gin.Context) {
+// @Summary Update Profile
+// @Description Update Profile
+// @Tags users
+// @Accept json
+// @Produce json
+// @Param user body users.UpdateProf true "Update Profile"
+// @Success 200 {object} users.UpdateProf
+// @Failure 400 {object} models.Error
+// @Router /users/updateProfile [put]
 
+func (h *Handler) UpdateProfile(c *gin.Context) {
+	req := users.UpdateProf{}
+
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Profile updated successfully!",
+	})
 }
 
 func (h *Handler) DeleteProfile(c *gin.Context) {
