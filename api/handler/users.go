17055dec58691a70f9b4d855fa78ba60ee0d85cf
pronium <azizbekqobulov05@gmail.com>
2024-07-10 19:14:47 +0500

    in progres..

--- api/handler/users.go
+++ api/handler/users.go
@@ -1,16 +1,31 @@
 package handler
 
-import "github.com/gin-gonic/gin"
+import (
+	"api_gateway/genproto/users"
+	"net/http"
 
+	"github.com/gin-gonic/gin"
+)
 
-func(h *Handler) GetProfile(c *gin.Context){
+func (h *Handler) GetProfile(c *gin.Context) {
 
 }
 
-func(h *Handler) UpdateProfile(c *gin.Context){
+func (h *Handler) UpdateProfile(c *gin.Context) {
+	req := users.UpdateProf{}
 
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Profile updated successfully!",
+	})
 }
 
-func(h *Handler) DeleteProfile(c *gin.Context){
-	
+func (h *Handler) DeleteProfile(c *gin.Context) {
+
 }
