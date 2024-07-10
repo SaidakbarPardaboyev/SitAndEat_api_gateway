c7c483e04c3b487381ac35953aa5070f280a43ea
Hamidjon Nuriddinov <nuriddinovhamidjon2@gmail.com>
2024-07-10 18:55:58 +0500

    add router

--- api/router.go
+++ api/router.go
@@ -16,4 +16,33 @@ func Router(conn *grpc.ClientConn)*gin.Engine{
 	users.GET("/getProfile/:id", h.GetProfile)
 	users.PUT("/updateProfile", h.UpdateProfile)
 	users.DELETE("/deleteProfile", h.DeleteProfile)
+
+	restaurant := router.Group("/restaurant")
+	restaurant.GET("/getRestaurant/:id", h.GetRestaurant)
+	restaurant.PUT("/updateRestaurant", h.UpdateRestaurant)
+	restaurant.DELETE("/deleteRestaurant/:id", h.DeleteRestaurant)
+	restaurant.POST("/createRestaurant", h.CreateRestaurant)
+	restaurant.GET("/getAllRestaurant", h.GetAllRestaurants)
+
+	reservation := router.Group("/reservation")
+	reservation.POST("/createReservation", h.CreateReservation)
+	reservation.GET("/getReservation/:id", h.GetReservation)
+	reservation.GET("/getAllReservations", h.GetAllReservation)
+	reservation.PUT("/updateReservation", h.UpdateReservations)
+	reservation.DELETE("/deleteReservation/:id", h.DeleteReservation)
+	reservation.GET("/getUserReservation/:id", h.GetReservationsByUserId)
+	reservation.POST("/orderMeal", h.OrderMeal)
+	reservation.POST("/payForReservation", h.PayForReservation)
+
+	menu := router.Group("/menu")
+	menu.POST("/createFood", h.CreateFood)
+	menu.GET("/getAllFoods", h.GetAllFoods)
+	menu.GET("/getFood/:id", h.GetFood)
+	menu.PUT("/updateFood", h.UpdateFood)
+	menu.DELETE("/deleteFood/:id", h.DeleteFood)
+
+	payment := router.Group("/payment")
+	payment.POST("/createPayment", h.CreatePayments)
+	payment.GET("/getPaymentStatus/:id", h.GetPaymentStatusById)
+	payment.PUT("/updatePayment", h.UpdatePayments)
 }
